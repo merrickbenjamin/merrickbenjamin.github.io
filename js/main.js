23222bac4b0a422833a7b518a9b4e70ab50d5058
Benjamin Merrick <merrick.benjamin@gmail.com>
2018-02-13 23:13:53 -0500

    portfolio styling WIP

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
 $(document).ready( function() {
 
-  $(".required.email").keyup( function() {
-    $(".user-type-radios").slideDown();
-  });
-
-  $("a[data-toggle='tab']").click(function() {
-    if ($(this).attr('href') == "#home") {
-      $(".site-wrapper").removeClass("no-bg");
-    } else {
-      $(".site-wrapper").addClass("no-bg");
-    }
-  });
-
   var hash = window.location.hash;
 
-  if (hash) {
-    var selectedTab = $('a[href="' + hash + '"]');
-    selectedTab.trigger('click', true);
+  if (hash == '#portfolio') {
+    //var selectedTab = $('a[href="' + hash + '"]');
+    //selectedTab.trigger('click', true);
+    $('#portfolio').modal({
+      'show': true,
+      'backdrop': 'static'
+    });
   }
 
 });
