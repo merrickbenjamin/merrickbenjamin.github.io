5398f73abb849ad11eec9fd3aa24b337b996fe91
Benjamin Merrick <merrick.benjamin@gmail.com>
2018-04-25 20:16:20 -0400

    portfolio v1 done

--- js/main.js
+++ js/main.js
@@ -13,9 +13,35 @@ $(document).ready( function() {
 
   //Hero img animation
   var hero = [
+    { e: $(".hero-img-container#austria"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#austria"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#autumn"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#autumn"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#boston"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#boston"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
     { e: $(".hero-img-container#fog"), p: "transition.slideUpIn", o: { duration: 1500 } },
-    { e: $(".hero-img-container#fog"), p: "transition.slideUpOut", o: { duration: 200 } },
-    { e: $(".hero-img-container#tenacity"), p: "transition.slideUpIn", o: { duration: 1500 } }
+    { e: $(".hero-img-container#fog"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#hk"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#hk"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#iceland1"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#iceland1"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#iceland2"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#iceland2"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#kauai"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#kauai"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#patagonia1"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#patagonia1"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#patagonia2"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#patagonia2"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#patagonia3"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#patagonia3"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#patagonia4"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#patagonia4"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#pattern"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#pattern"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#tunnel"), p: "transition.slideUpIn", o: { duration: 1500 } },
+    { e: $(".hero-img-container#tunnel"), p: "transition.slideUpOut", o: { duration: 200, delay: 3000 } },
+    { e: $(".hero-img-container#valparaiso"), p: "transition.slideUpIn", o: { duration: 300 } }
   ];
   $.Velocity.RunSequence(hero);
 
